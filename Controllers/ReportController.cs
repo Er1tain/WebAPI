85a5b38558d8a717eaea01d2c0a6e3a4ff585417
Alexander Pridava <sanyapridava@mail.ru>
2024-04-04 16:41:32 +0300

    Create protype class whick definition data request/response

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -6,6 +6,20 @@ namespace SobesWebAPI.Controllers;
 
 public class ReportController: Controller
 {
+    //RequestData for POST/report/user_statistics
+    private record class User(
+            string id_user,
+            int start,
+            int end    
+        );
+    
+    //ResponseData for GET/report info
+    private record class Query(
+        string GUID,
+        int percent,
+        
+        );
+    
     [HttpPost]
     public string User_statistics()
     {
