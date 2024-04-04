ff5f1dc9fed10c2593d6e2a6cf0fa39b05da528d
Alexander Pridava <sanyapridava@mail.ru>
2024-04-04 16:04:48 +0300

    First commit

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SobesWebAPI.Models;
+
+namespace SobesWebAPI.Controllers;
+
+public class ReportController: Controller
+{
+    
+    
+    
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SobesWebAPI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
