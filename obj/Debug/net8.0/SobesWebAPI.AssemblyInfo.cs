79c98be144eaefc1d6c6c4ea811b54759e12b583
Alexander Pridava <sanyapridava@mail.ru>
2024-04-05 14:00:17 +0300

    System info

--- obj/Debug/net8.0/SobesWebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SobesWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SobesWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc85f7603b36ac35b98c960cc94b8101401a689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e133d10c59a987996b81666ed72ba9f0e34d581a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SobesWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SobesWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
